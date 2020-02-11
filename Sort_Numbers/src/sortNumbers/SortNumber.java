77c8d9b5affc507072b11525eeebf2b172801b1f
Dayoon Lee <danadayoonlee@gmail.com>
2020-02-11 18:22:46 -0500

    initial commit

---
+++ Sort_Numbers/src/sortNumbers/SortNumber.java
@@ -0,0 +1,17 @@
+package sortNumbers;
+
+import java.util.Arrays;
+
+public class SortNumber {
+	public static void SortNumbers(int... numbers) {
+		
+		Arrays.sort(numbers);
+		
+		System.out.print(" Sorted array in descending order: ");
+		
+		for (int i = numbers.length-1; 0 <= i; i--) {
+		      System.out.printf(numbers[i] + " ");
+		}
+		System.out.println();
+	}
+}
---
+++ Sort_Numbers/src/sortNumbers/SortNumberTest.java
@@ -0,0 +1,16 @@
+package sortNumbers;
+
+public class SortNumberTest {
+
+	public static void main(String[] args) {
+
+		int number1 = 10;
+		int number2 = 20;
+		int number3 = 30;
+		int number4 = 40;
+		int number5 = 50;
+	    
+		SortNumber.SortNumbers(number1, number2, number3, number4, number5);
+	}
+	
+}
